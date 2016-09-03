ae319c07085a574cdb876fe6b840cd8769c7f45e
Rafal Magda <rafalmag@gmail.com>
2016-09-03 23:38:25 +0200

    wear standalone gui - work in progress - no NPE

--- wear/src/main/java/test/android/rafalmag/pl/worktimetracker/WearActivity.java
+++ wear/src/main/java/test/android/rafalmag/pl/worktimetracker/WearActivity.java
@@ -1,12 +1,8 @@
 package test.android.rafalmag.pl.worktimetracker;
 
 import android.app.Activity;
-import android.content.Context;
 import android.os.Bundle;
-import android.support.wearable.view.WatchViewStub;
-import android.util.AttributeSet;
-import android.view.View;
-import android.widget.TextView;
+import android.support.wearable.view.BoxInsetLayout;
 import android.widget.TimePicker;
 
 import butterknife.BindView;
@@ -14,14 +10,14 @@ import butterknife.ButterKnife;
 
 public class WearActivity extends Activity {
 
-    // TODO NullPointerException: Attempt to invoke virtual method 'android.view.View android.view.View.findViewById(int)' on a null object reference
-//    @BindView(R.id.timePicker)
-//    TimePicker timePicker;
+    @BindView(R.id.timePicker)
+    TimePicker timePicker;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_wear);
+        ButterKnife.bind(this);
 //        final WatchViewStub stub = (WatchViewStub) findViewById(R.id.watch_view_stub);
 //        stub.setOnLayoutInflatedListener(new WatchViewStub.OnLayoutInflatedListener() {
 //            @Override
@@ -29,16 +25,7 @@ public class WearActivity extends Activity {
 //                mTextView = (TextView) stub.findViewById(R.id.text);
 //            }
 //        });
+        timePicker.setIs24HourView(true);
     }
 
-    @Override
-    public View onCreateView(View parent, String name, Context context, AttributeSet attrs) {
-        View view = super.onCreateView(parent, name, context, attrs);
-//        ButterKnife.bind(this, view);
-//        timePicker = (TimePicker) findViewById(R.id.timePicker);
-        // 12h is too big for wear
-        // TODO NPE
-//        timePicker.setIs24HourView(true);
-        return view;
-    }
 }
