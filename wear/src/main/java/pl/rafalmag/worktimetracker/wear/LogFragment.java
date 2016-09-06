e12d44e9e306cfa37a0c21b139c9cead946a5074
Rafal Magda <rafalmag@gmail.com>
2016-09-06 20:26:09 +0200

    logFragment NPE fixed ; BTW: mobile tested

--- wear/src/main/java/pl/rafalmag/worktimetracker/wear/LogFragment.java
+++ wear/src/main/java/pl/rafalmag/worktimetracker/wear/LogFragment.java
@@ -5,6 +5,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.wearable.activity.ConfirmationActivity;
 import android.support.wearable.view.DelayedConfirmationView;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -15,12 +16,14 @@ import butterknife.ButterKnife;
 
 public class LogFragment extends Fragment {
 
+    private static final String TAG = LogFragment.class.getCanonicalName();
+
     @BindView(R.id.delayed_confirm)
     DelayedConfirmationView mDelayedView;
 
     // icon from https://design.google.com/icons/
     // https://books.google.pl/books?id=OOkmCAAAQBAJ&pg=PA148&lpg=PA148&dq=android+wear+confirmation+example&source=bl&ots=USfV9lASYl&sig=agiOXpu8T5LesYidZ6SHfuBQFZ0&hl=en&sa=X&ved=0ahUKEwiF5ZrosvXOAhXsHJoKHdWOCXoQ6AEIUTAJ#v=onepage&q=android%20wear%20confirmation%20example&f=false
-
+    boolean mDelayedViewRunning = false;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
@@ -31,34 +34,41 @@ public class LogFragment extends Fragment {
         mDelayedView.setListener(new DelayedConfirmationView.DelayedConfirmationListener() {
             @Override
             public void onTimerFinished(View view) {
-                running = false;
-                // User didn't cancel, perform the action
-                //FIXME NPE - getActivity() can have null context when scrolled out... handle it like cancel!
-                Intent intent = new Intent(getActivity(), ConfirmationActivity.class);
-                intent.putExtra(ConfirmationActivity.EXTRA_ANIMATION_TYPE, ConfirmationActivity.SUCCESS_ANIMATION);
-                intent.putExtra(ConfirmationActivity.EXTRA_MESSAGE, getString(R.string.time_logged));
-                startActivity(intent);
+                if(mDelayedViewRunning) {
+                    // User didn't cancel, perform the action
+                    Intent intent = new Intent(getActivity(), ConfirmationActivity.class);
+                    intent.putExtra(ConfirmationActivity.EXTRA_ANIMATION_TYPE, ConfirmationActivity.SUCCESS_ANIMATION);
+                    intent.putExtra(ConfirmationActivity.EXTRA_MESSAGE, getString(R.string.time_logged));
+                    startActivity(intent);
+                    //TODO go to first page
+                }else {
+                    Log.d(TAG,"timer finished, but delayedView is not running - can happen when user scrolls out = cancel");
+                }
                 mDelayedView.reset();
-                //TODO go to first page
+                mDelayedViewRunning = false;
             }
-
-            boolean running = false;
 
             @Override
             public void onTimerSelected(View view) {
-                if (running) {
+                if (mDelayedViewRunning) {
                     // User canceled, abort the action
                     mDelayedView.reset();
-                    running = false;
+                    mDelayedViewRunning = false;
                 } else {
                     mDelayedView.setTotalTimeMs(2000);
                     // Start the timer
                     mDelayedView.start();
-                    running = true;
+                    mDelayedViewRunning = true;
                 }
             }
         });
         return view;
     }
 
+    @Override
+    public void onPause() {
+        super.onPause();
+        mDelayedViewRunning = false;
+        mDelayedView.reset();
+    }
 }
