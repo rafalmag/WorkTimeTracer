14ad65fa86f0357a17e3f77da3c5d0b0caa73a32
Rafal Magda <rafalmag@gmail.com>
2017-01-08 22:35:32 +0100

    wear connect laptop dirs

--- wear/connect.bat
+++ wear/connect.bat
@@ -1,3 +1,5 @@
+rem laptop
+rem C:\Android\sdk\platform-tools
 cd C:\PF\adt-bundle-windows-x86_64-20131030\sdk\platform-tools
 adb forward tcp:4444 localabstract:/adb-hub
 adb connect 127.0.0.1:4444
